4607a3b62bcfb8d56605f2b36645a46d14efa295
Laurent Itti <jevois.org@gmail.com>
2017-05-31 12:22:52 -0700

    Basic skeleton of python support working on host. We can now define new JeVois module in python.

---
+++ src/Modules/PythonTest/PythonTest.py
@@ -0,0 +1,17 @@
+class PythonTest:
+    # Constructor
+    def __init__(self):
+        print("Constructor")
+
+    # Process function with no USB output
+    def process1(self, inframe):
+        print("process no usb")
+
+    # Process function with USB output
+    def process2(self, inframe, outframe):
+        print("process with usb")
+
+    # test hello function
+    def hello(self):
+        print("hello there from python")
+
