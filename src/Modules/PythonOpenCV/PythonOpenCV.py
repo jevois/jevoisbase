dfc82457f82e9deb7f3ad866641a8ec986021979
Laurent Itti <jevois.org@gmail.com>
2017-06-01 11:33:10 -0700

    Edge detection example nice and clean now using newly defined constants in the jevois module

--- src/Modules/PythonOpenCV/PythonOpenCV.py
+++ src/Modules/PythonOpenCV/PythonOpenCV.py
@@ -8,7 +8,8 @@ class PythonOpenCV:
     ####################################################################################################
     def __init__(self):
         print("Constructor")
-
+        self.timer = jevois.Timer("canny", 100, jevois.LOG_INFO)
+        
     ####################################################################################################
     # Process function with no USB output
     ####################################################################################################
@@ -19,11 +20,11 @@ class PythonOpenCV:
     # Process function with USB output
     ####################################################################################################
     def process(self, inframe, outframe):
-        print("process with usb")
+        self.timer.start()
 
         # Get the next camera image (may block until it is captured):
         inimg = inframe.get()
-        print("Input image is {} {}x{}".format(jevois.fccstr(inimg.fmt), inimg.width, inimg.height))
+        #jevois.LINFO("Input image is {} {}x{}".format(jevois.fccstr(inimg.fmt), inimg.width, inimg.height))
 
         # Convert the input image to OpenCV grayscale:
         inimggray = jevois.convertToCvGray(inimg);
@@ -33,14 +34,10 @@ class PythonOpenCV:
 
         # Get the next available USB output image:
         outimg = outframe.get()
-        print("Output image is {} {}x{}".format(jevois.fccstr(outimg.fmt), outimg.width, outimg.height))
+        #jevois.LINFO("Output image is {} {}x{}".format(jevois.fccstr(outimg.fmt), outimg.width, outimg.height))
 
         # Require that output image has same dims as input and is grayscale:
-        # FIXME need values for V$L2_PIX_...
-        
-        # Reinterpret the output image as an OpenCV image (does not copy the pixel data):
-        # THE PIXEL SHARING DOES NOT SEEM TO WORK
-        #outimgcv = jevois.cvImage(outimg)
+        outimg.require("output", inimg.width, inimg.height, jevois.V4L2_PIX_FMT_GREY);
         
         # Detect edges using the Canny algorithm from OpenCV and stuff the results into our outimgcv, and hence directly
         # into outimg since the pixel data is shared:
@@ -48,6 +45,10 @@ class PythonOpenCV:
 
         # Copy the edge map to the output image buffer to send over USB:
         jevois.convertCvGRAYtoRawImage(edges, outimg, 100)
+
+        # Write frames/s info from our timer:
+        fps = self.timer.stop();
+        jevois.writeText(outimg, fps, 3, outimg.height-13, jevois.YUYV.White, jevois.Font.Font6x10)
         
         # We are done with the output, ready to send it to host over USB:
         outframe.send()
