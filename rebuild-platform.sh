fb45aa5bb4c4846263b808b672717fc23d0ba987
Laurent Itti <jevois.org@gmail.com>
2017-06-02 09:24:27 -0700

    shorter build target choice option. Will add a direct build to SD soon now that the SD is visible as a USB drive.

--- rebuild-platform.sh
+++ rebuild-platform.sh
@@ -1,4 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+# USAGE: rebuild-platform.sh [-b] [cmake opts]
+
+# if -b is specified as first argument, install modules to buildroot for later packing to microSD by jevois-sdk
+# otherwise, install to jvpkg/ in jevoisbase (for later make jvpkg)
+
+extra=""
+if [ "X$1" = "X-b" ]; then extra="-DJEVOIS_MODULES_TO_BUILDROOT=ON"; shift; fi
+
 
 # you can specify -DJEVOIS_MODULES_TO_BUILDROOT=ON to install directly to buildroot, thereby bypassing the need to build
 # a .jvpkg package that will then be unpacked by the smart camera
@@ -7,7 +15,7 @@
 ncpu=`cat /proc/cpuinfo |grep processor|wc -l`
 if [ `cat /proc/cpuinfo | grep ARM | wc -l` -gt 0 ]; then ncpu=1; fi
 
-/bin/rm -rf pbuild && mkdir pbuild && cd pbuild && cmake "$@" -DJEVOIS_PLATFORM=ON .. && make -j ${ncpu} && make install
+/bin/rm -rf pbuild && mkdir pbuild && cd pbuild && cmake "${extra} $@" -DJEVOIS_PLATFORM=ON .. && make -j ${ncpu} && make install
 
 
 # avoid clogging up the SD card with tiny-cnn training data:
