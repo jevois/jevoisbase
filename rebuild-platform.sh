2d4894b7ee4535671bd3db2fc163e28ef880b5de
Laurent Itti <jevois.org@gmail.com>
2017-06-07 15:17:54 -0700

    finalizing the new file organization which allows us to create debian packages

--- rebuild-platform.sh
+++ rebuild-platform.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# USAGE: rebuild-platform.sh [-b] [cmake opts]
+# USAGE: rebuild-platform.sh [--staging|--microsd /dev/sdX] [cmake opts]
 
 # if -b is specified as first argument, install modules to buildroot for later packing to microSD by jevois-sdk
 # otherwise, install to jvpkg/ in jevoisbase (for later make jvpkg)
 
 extra=""
-if [ "X$1" = "X-b" ]; then extra="-DJEVOIS_MODULES_TO_BUILDROOT=ON"; shift; fi
+if [ "X$1" = "X--staging" ]; then extra="-DJEVOIS_MODULES_TO_STAGING=ON"; shift; fi
+##TODO if [ "X$1" = "X--microsd" ]; then extra="-DJEVOIS_MODULES_TO_STAGING=ON"; shift; fi
 
 
 # you can specify -DJEVOIS_MODULES_TO_BUILDROOT=ON to install directly to buildroot, thereby bypassing the need to build
@@ -15,10 +16,5 @@ if [ "X$1" = "X-b" ]; then extra="-DJEVOIS_MODULES_TO_BUILDROOT=ON"; shift; fi
 ncpu=`cat /proc/cpuinfo |grep processor|wc -l`
 if [ `cat /proc/cpuinfo | grep ARM | wc -l` -gt 0 ]; then ncpu=1; fi
 
-/bin/rm -rf pbuild && mkdir pbuild && cd pbuild && cmake "${extra} $@" -DJEVOIS_PLATFORM=ON .. && make -j ${ncpu} && make install
+/bin/rm -rf pbuild && mkdir pbuild && cd pbuild && cmake "${extra} $@" -DJEVOIS_PLATFORM=ON .. && make -j ${ncpu} && sudo make install
 
-
-# avoid clogging up the SD card with tiny-cnn training data:
-/bin/rm -f ${JEVOIS_SRC_ROOT}/jevois-sdk/out/sun8iw5p1/linux/common/buildroot/target/jevois/modules/*/JeVois/*/tiny-dnn/*/*.bin
-
-/bin/rm -f ${JEVOIS_SRC_ROOT}/jevois-sdk/out/sun8iw5p1/linux/common/buildroot/target/jevois/modules/*/JeVois/*/tiny-dnn/*/*ubyte
