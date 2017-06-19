6ee9c1ea41293ec174437d7c801df74b0e1b3a32
Laurent Itti <jevois.org@gmail.com>
2017-06-19 14:34:22 -0700

    added target --live to rebuild-platform which will export the microSD inside jevois, install, and eject and reboot. Script seems a bit brittle but works ok on my host.

--- rebuild-platform.sh
+++ rebuild-platform.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-# USAGE: rebuild-platform.sh [--staging|--microsd] [cmake opts]
+# USAGE: rebuild-platform.sh [--staging|--microsd|--live] [cmake opts]
 
 # OPTIONS:
-# [none] - install compiled modules to jvpkg/ for later packing by make jvpkg
+# [none]    - install compiled modules to jvpkg/ for later packing by make jvpkg
 # --staging - install to microSD staging area /var/lib/jevois-microsd/
-# --microsd - install directly to live microSD card which should be mounted under /media/username/JEVOIS
+# --microsd - install directly to microSD card inserted into host computer
+# --live    - install directly to microSD card inside running JeVois connected to host computer
 
 extra=""
 if [ "X$1" = "X--staging" ]; then extra="-DJEVOIS_MODULES_TO_STAGING=ON"; shift; fi
-if [ "X$1" = "X--microsd" ]; then
-    /bin/ls /media/${USER}/JEVOIS/ >/dev/null 2>&1
-    if [ $? -ne 0 ]; then
-	echo "Cannot read /media/${USER}/JEVOIS - insert microSD into host computer "
-	echo "       or run command 'usbsd' in the JeVois command line interface."
-	echo "ABORT"
-	exit 1
-    fi
-    extra="-DJEVOIS_MODULES_TO_MICROSD=ON";
-    shift;
-fi
+if [ "X$1" = "X--microsd" ]; then extra="-DJEVOIS_MODULES_TO_MICROSD=ON"; shift; fi
+if [ "X$1" = "X--live" ]; then extra="-DJEVOIS_MODULES_TO_LIVE=ON"; shift; fi
 
 # On ARM hosts like Raspberry Pi3, we will likely run out of memory if attempting more than 1 compilation thread:
 ncpu=`cat /proc/cpuinfo |grep processor|wc -l`
