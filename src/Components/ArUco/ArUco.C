8853266f7e580be2ae5708a2f1b2b93cf4364c8e
Laurent Itti <jevois.org@gmail.com>
2017-07-19 17:10:12 -0700

    refactoring some ArUco code to follow the model of ARtoolkit

--- src/Components/ArUco/ArUco.C
+++ src/Components/ArUco/ArUco.C
@@ -16,7 +16,11 @@
 /*! \file */
 
 #include <jevoisbase/Components/ArUco/ArUco.H>
-#include <string>
+#include <jevois/Image/RawImageOps.H>
+#include <opencv2/calib3d.hpp> // for projectPoints()
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <Eigen/Geometry> // for AngleAxis and Quaternion
 
 // ##############################################################################################################
 ArUco::~ArUco()
@@ -120,10 +124,152 @@ void ArUco::detectMarkers(cv::InputArray image, cv::OutputArray ids, cv::OutputA
 }
 
 // ##############################################################################################################
-void ArUco::estimatePoseSingleMarkers(cv::InputArrayOfArrays corners, float markerLength,
-                                      cv::OutputArray rvecs, cv::OutputArray tvecs)
+void ArUco::estimatePoseSingleMarkers(cv::InputArrayOfArrays corners, cv::OutputArray rvecs, cv::OutputArray tvecs)
 {
-  cv::aruco::estimatePoseSingleMarkers(corners, markerLength, itsCamMatrix, itsDistCoeffs, rvecs, tvecs);
+  cv::aruco::estimatePoseSingleMarkers(corners, markerlen::get(), itsCamMatrix, itsDistCoeffs, rvecs, tvecs);
 }
 
+
+// ##############################################################################################################
+void ArUco::sendSerial(jevois::StdModule * mod, std::vector<int> ids, std::vector<std::vector<cv::Point2f> > corners,
+                       unsigned int w, unsigned int h, std::vector<cv::Vec3d> const & rvecs,
+                       std::vector<cv::Vec3d> const & tvecs)
+{
+  if (rvecs.empty() == false)
+  {
+    float const siz = markerlen::get();
+    
+    // If we have rvecs and tvecs, we are doing 3D pose estimation, so send a 3D message:
+    for (size_t i = 0; i < corners.size(); ++i)
+    {
+      std::vector<cv::Point2f> const & currentMarker = corners[i];
+      cv::Vec3d const & rv = rvecs[i];
+      cv::Vec3d const & tv = tvecs[i];
+      
+      // Compute quaternion:
+      float theta = std::sqrt(rv[0] * rv[0] + rv[1] * rv[1] + rv[2] * rv[2]);
+      Eigen::Vector3f axis(rv[0], rv[1], rv[2]);
+      Eigen::Quaternion<float> q(Eigen::AngleAxis<float>(theta, axis));
+      
+      mod->sendSerialStd3D(tv[0], tv[1], tv[2],        // position
+                           siz, siz, 1.0F,             // size
+                           q.w(), q.x(), q.y(), q.z(), // pose
+                           std::to_string(ids[i]));    // decoded ID
+    }
+  }
+  else
+  {
+    // Send one 2D message per parker:
+    for (size_t i = 0; i < corners.size(); ++i)
+    {
+      std::vector<cv::Point2f> const & currentMarker = corners[i];
+      mod->sendSerialContour2D(w, h, currentMarker, std::to_string(ids[i]));
+    }
+  }
+}
+
+// ##############################################################################################################
+void ArUco::drawDetections(jevois::RawImage & outimg, int txtx, int txty, std::vector<int> ids,
+                           std::vector<std::vector<cv::Point2f> > corners, std::vector<cv::Vec3d> const & rvecs,
+                           std::vector<cv::Vec3d> const & tvecs)
+{
+  // This code is like drawDetectedMarkers() in cv::aruco, but for YUYV output image:
+  int nMarkers = int(corners.size());
+  for (int i = 0; i < nMarkers; ++i)
+  {
+    std::vector<cv::Point2f> const & currentMarker = corners[i];
+        
+    // draw marker sides and prepare serial out string:
+    for (int j = 0; j < 4; ++j)
+    {
+      cv::Point2f const & p0 = currentMarker[j];
+      cv::Point2f const & p1 = currentMarker[ (j+1) % 4 ];
+      jevois::rawimage::drawLine(outimg, int(p0.x + 0.5F), int(p0.y + 0.5F),
+                                 int(p1.x + 0.5F), int(p1.y + 0.5F), 1, jevois::yuyv::LightGreen);
+    }
+        
+    // draw first corner mark
+    jevois::rawimage::drawDisk(outimg, int(currentMarker[0].x + 0.5F), int(currentMarker[0].y + 0.5F),
+                               3, jevois::yuyv::LightGreen);
+
+    // draw ID
+    if (ids.empty() == false)
+    {
+      cv::Point2f cent(0.0F, 0.0F); for (int p = 0; p < 4; ++p) cent += currentMarker[p] * 0.25F;
+      jevois::rawimage::writeText(outimg, std::string("id=") + std::to_string(ids[i]),
+                                  int(cent.x + 0.5F), int(cent.y + 0.5F) - 5, jevois::yuyv::LightGreen);
+    }
+  }
+
+  // This code is like drawAxis() in cv::aruco, but for YUYV output image:
+  if (dopose::get() && ids.empty() == false)
+  {
+    float const length = markerlen::get() * 0.4F;
+        
+    for (size_t i = 0; i < ids.size(); ++i)
+    {
+      // Project axis points:
+      std::vector<cv::Point3f> axisPoints;
+      axisPoints.push_back(cv::Point3f(0.0F, 0.0F, 0.0F));
+      axisPoints.push_back(cv::Point3f(length, 0.0F, 0.0F));
+      axisPoints.push_back(cv::Point3f(0.0F, length, 0.0F));
+      axisPoints.push_back(cv::Point3f(0.0F, 0.0F, length));
+      
+      std::vector<cv::Point2f> imagePoints;
+      cv::projectPoints(axisPoints, rvecs[i], tvecs[i], itsCamMatrix, itsDistCoeffs, imagePoints);
+      
+      // Draw axis lines
+      jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
+                                 int(imagePoints[1].x + 0.5F), int(imagePoints[1].y + 0.5F),
+                                 2, jevois::yuyv::MedPurple);
+      jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
+                                 int(imagePoints[2].x + 0.5F), int(imagePoints[2].y + 0.5F),
+                                 2, jevois::yuyv::MedGreen);
+      jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
+                                 int(imagePoints[3].x + 0.5F), int(imagePoints[3].y + 0.5F),
+                                 2, jevois::yuyv::MedGrey);
+      
+      // Also draw a cube if requested:
+      if (showcube::get())
+      {
+        float const len = markerlen::get() * 0.5F;
+        
+        std::vector<cv::Point3f> cubePoints;
+        cubePoints.push_back(cv::Point3f(-len, -len, 0.0F));
+        cubePoints.push_back(cv::Point3f(len, -len, 0.0F));
+        cubePoints.push_back(cv::Point3f(len, len, 0.0F));
+        cubePoints.push_back(cv::Point3f(-len, len, 0.0F));
+        cubePoints.push_back(cv::Point3f(-len, -len, len * 2.0F));
+        cubePoints.push_back(cv::Point3f(len, -len, len * 2.0F));
+        cubePoints.push_back(cv::Point3f(len, len, len * 2.0F));
+        cubePoints.push_back(cv::Point3f(-len, len, len * 2.0F));
+        
+        std::vector<cv::Point2f> cuf;
+        cv::projectPoints(cubePoints, rvecs[i], tvecs[i], itsCamMatrix, itsDistCoeffs, cuf);
+        
+        // Round all the coordinates:
+        std::vector<cv::Point> cu;
+        for (auto const & p : cuf) cu.push_back(cv::Point(int(p.x + 0.5F), int(p.y + 0.5F)));
+        
+        // Draw cube lines:
+        jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[1].x, cu[1].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[2].x, cu[2].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[3].x, cu[3].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[0].x, cu[0].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[4].x, cu[4].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[5].x, cu[5].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[6].x, cu[6].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[7].x, cu[7].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
+      }
+      
+    }
+  }
+  
+  jevois::rawimage::writeText(outimg, "Detected " + std::to_string(ids.size()) + " ArUco markers.",
+                              txtx, txty, jevois::yuyv::White);
+}
 
--- src/Modules/DemoArUco/DemoArUco.C
+++ src/Modules/DemoArUco/DemoArUco.C
@@ -18,27 +18,8 @@
 #include <jevois/Core/Module.H>
 #include <jevois/Debug/Timer.H>
 #include <jevois/Image/RawImageOps.H>
-#include <jevois/Util/Coordinates.H>
 #include <jevoisbase/Components/ArUco/ArUco.H>
-#include <linux/videodev2.h> // for v4l2 pixel types
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/calib3d.hpp> // for projectPoints()
-#include <sstream>
-
-#include <Eigen/Geometry> // for AngleAxis and Quaternion
-
-//! Parameter \relates DemoArUco
-JEVOIS_DECLARE_PARAMETER(dopose, bool, "Compute and show pose vectors, requires a valid camera calibration",
-                         false, aruco::ParamCateg);
-
-//! Parameter \relates DemoArUco
-JEVOIS_DECLARE_PARAMETER(markerlen, float, "Marker side length (millimeters), used only for pose estimation",
-                         100.0F, aruco::ParamCateg);
-
-//! Parameter \relates DemoArUco
-JEVOIS_DECLARE_PARAMETER(showcube, bool, "Show a 3D cube on top of each detected marker, when dopose is also true",
-                         true, aruco::ParamCateg);
 
 //! Simple demo of ArUco augmented reality markers detection and decoding
 /*! Detect and decode patterns known as ArUco markers, which are small 2D barcodes often used in augmented
@@ -146,8 +127,7 @@ JEVOIS_DECLARE_PARAMETER(showcube, bool, "Show a 3D cube on top of each detected
     @distribution Unrestricted
     @restrictions None
     \ingroup modules */
-class DemoArUco : public jevois::StdModule,
-                  public jevois::Parameter<dopose, markerlen, showcube>
+class DemoArUco : public jevois::StdModule
 {
   public: 
     // ####################################################################################################
@@ -180,56 +160,16 @@ class DemoArUco : public jevois::StdModule,
 
       // Do pose computation if desired:
       std::vector<cv::Vec3d> rvecs, tvecs;
-      if (dopose::get() && ids.empty() == false)
-        itsArUco->estimatePoseSingleMarkers(corners, markerlen::get(), rvecs, tvecs);
+      if (itsArUco->dopose::get() && ids.empty() == false)
+        itsArUco->estimatePoseSingleMarkers(corners, rvecs, tvecs);
       
       // Let camera know we are done processing the input image:
       inframe.done();
 
       // Send serial output:
-      sendAllSerial(ids, corners, w, h, rvecs, tvecs);
+      itsArUco->sendSerial(this, ids, corners, w, h, rvecs, tvecs);
     }
 
-    // ####################################################################################################
-    //! Send the serial messages
-    // ####################################################################################################
-    void sendAllSerial(std::vector<int> ids, std::vector<std::vector<cv::Point2f> > corners,
-                       unsigned int w, unsigned int h, std::vector<cv::Vec3d> const & rvecs,
-                       std::vector<cv::Vec3d> const & tvecs)
-    {
-      if (rvecs.empty() == false)
-      {
-        float const siz = markerlen::get();
-        
-        // If we have rvecs and tvecs, we are doing 3D pose estimation, so send a 3D message:
-        for (size_t i = 0; i < corners.size(); ++i)
-        {
-          std::vector<cv::Point2f> const & currentMarker = corners[i];
-          cv::Vec3d const & rv = rvecs[i];
-          cv::Vec3d const & tv = tvecs[i];
-          
-          // Compute quaternion:
-          float theta = std::sqrt(rv[0] * rv[0] + rv[1] * rv[1] + rv[2] * rv[2]);
-          Eigen::Vector3f axis(rv[0], rv[1], rv[2]);
-          Eigen::Quaternion<float> q(Eigen::AngleAxis<float>(theta, axis));
-          
-          sendSerialStd3D(tv[0], tv[1], tv[2],        // position
-                          siz, siz, 1.0F,             // size
-                          q.w(), q.x(), q.y(), q.z(), // pose
-                          std::to_string(ids[i]));    // decoded ID
-        }
-      }
-      else
-      {
-        // Send one 2D message per parker:
-        for (size_t i = 0; i < corners.size(); ++i)
-        {
-          std::vector<cv::Point2f> const & currentMarker = corners[i];
-          sendSerialContour2D(w, h, currentMarker, std::to_string(ids[i]));
-        }
-      }
-    }
-    
     // ####################################################################################################
     //! Processing function with video output to USB
     // ####################################################################################################
@@ -263,8 +203,8 @@ class DemoArUco : public jevois::StdModule,
       std::vector<cv::Vec3d> rvecs, tvecs;
       itsArUco->detectMarkers(cvimg, ids, corners);
 
-      if (dopose::get() && ids.empty() == false)
-        itsArUco->estimatePoseSingleMarkers(corners, markerlen::get(), rvecs, tvecs);
+      if (itsArUco->dopose::get() && ids.empty() == false)
+        itsArUco->estimatePoseSingleMarkers(corners, rvecs, tvecs);
       
       // Wait for paste to finish up:
       paste_fut.get();
@@ -273,110 +213,10 @@ class DemoArUco : public jevois::StdModule,
       inframe.done();
 
       // Show all the results:
-
-      // This code is like drawDetectedMarkers() in cv::aruco, but for YUYV output image:
-      int nMarkers = int(corners.size());
-      for (int i = 0; i < nMarkers; ++i)
-      {
-        std::vector<cv::Point2f> const & currentMarker = corners[i];
-        
-        // draw marker sides and prepare serial out string:
-        for (int j = 0; j < 4; ++j)
-        {
-          cv::Point2f const & p0 = currentMarker[j];
-          cv::Point2f const & p1 = currentMarker[ (j+1) % 4 ];
-          jevois::rawimage::drawLine(outimg, int(p0.x + 0.5F), int(p0.y + 0.5F),
-                                     int(p1.x + 0.5F), int(p1.y + 0.3999F), 1, jevois::yuyv::LightGreen);
-        }
-        
-        // draw first corner mark
-        jevois::rawimage::drawDisk(outimg, int(currentMarker[0].x + 0.5F), int(currentMarker[0].y + 0.5F),
-                                   3, jevois::yuyv::LightGreen);
-
-        // draw ID
-        if (ids.empty() == false)
-        {
-          cv::Point2f cent(0.0F, 0.0F); for (int p = 0; p < 4; ++p) cent += currentMarker[p] * 0.25F;
-          jevois::rawimage::writeText(outimg, std::string("id=") + std::to_string(ids[i]),
-                                      int(cent.x + 0.5F), int(cent.y + 0.5F) - 5, jevois::yuyv::LightPink);
-        }
-      }
+      itsArUco->drawDetections(outimg, 3, h+5, ids, corners, rvecs, tvecs);
 
       // Send serial output:
-      sendAllSerial(ids, corners, w, h, rvecs, tvecs);
-
-      // This code is like drawAxis() in cv::aruco, but for YUYV output image:
-      if (dopose::get() && ids.empty() == false)
-      {
-        float const length = markerlen::get() * 0.4F;
-        
-        for (size_t i = 0; i < ids.size(); ++i)
-        {
-          // Project axis points:
-          std::vector<cv::Point3f> axisPoints;
-          axisPoints.push_back(cv::Point3f(0.0F, 0.0F, 0.0F));
-          axisPoints.push_back(cv::Point3f(length, 0.0F, 0.0F));
-          axisPoints.push_back(cv::Point3f(0.0F, length, 0.0F));
-          axisPoints.push_back(cv::Point3f(0.0F, 0.0F, length));
-
-          std::vector<cv::Point2f> imagePoints;
-          cv::projectPoints(axisPoints, rvecs[i], tvecs[i], itsArUco->itsCamMatrix,
-                            itsArUco->itsDistCoeffs, imagePoints);
-
-          // Draw axis lines
-          jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
-                                     int(imagePoints[1].x + 0.5F), int(imagePoints[1].y + 0.5F),
-                                     2, jevois::yuyv::MedPurple);
-          jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
-                                     int(imagePoints[2].x + 0.5F), int(imagePoints[2].y + 0.5F),
-                                     2, jevois::yuyv::MedGreen);
-          jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
-                                     int(imagePoints[3].x + 0.5F), int(imagePoints[3].y + 0.5F),
-                                     2, jevois::yuyv::MedGrey);
-
-          // Also draw a cube if requested:
-          if (showcube::get())
-          {
-            float const len = markerlen::get() * 0.5F;
-
-            std::vector<cv::Point3f> cubePoints;
-            cubePoints.push_back(cv::Point3f(-len, -len, 0.0F));
-            cubePoints.push_back(cv::Point3f(len, -len, 0.0F));
-            cubePoints.push_back(cv::Point3f(len, len, 0.0F));
-            cubePoints.push_back(cv::Point3f(-len, len, 0.0F));
-            cubePoints.push_back(cv::Point3f(-len, -len, len * 2.0F));
-            cubePoints.push_back(cv::Point3f(len, -len, len * 2.0F));
-            cubePoints.push_back(cv::Point3f(len, len, len * 2.0F));
-            cubePoints.push_back(cv::Point3f(-len, len, len * 2.0F));
-
-            std::vector<cv::Point2f> cuf;
-            cv::projectPoints(cubePoints, rvecs[i], tvecs[i], itsArUco->itsCamMatrix,
-                              itsArUco->itsDistCoeffs, cuf);
-
-            // Round all the coordinates:
-            std::vector<cv::Point> cu;
-            for (auto const & p : cuf) cu.push_back(cv::Point(int(p.x + 0.5F), int(p.y + 0.5F)));
-            
-            // Draw cube lines:
-            jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[1].x, cu[1].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[2].x, cu[2].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[3].x, cu[3].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[0].x, cu[0].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[4].x, cu[4].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[5].x, cu[5].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[6].x, cu[6].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[7].x, cu[7].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
-            jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
-          }
-          
-        }
-      }
-
-      jevois::rawimage::writeText(outimg, "Detected " + std::to_string(ids.size()) + " ArUco markers.",
-                                  3, h + 5, jevois::yuyv::White);
+      itsArUco->sendSerial(this, ids, corners, w, h, rvecs, tvecs);
 
       // Show processing fps:
       std::string const & fpscpu = timer.stop();
