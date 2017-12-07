188ce74eb02374dd2d2a37d902d6f0de1b6d1fba
Laurent Itti <jevois.org@gmail.com>
2017-12-07 04:27:25 -0800

    better exception handling while we are loading a deep network

--- src/Modules/DarknetYOLO/DarknetYOLO.C
+++ src/Modules/DarknetYOLO/DarknetYOLO.C
@@ -293,45 +293,50 @@ class DarknetYOLO : public jevois::StdModule,
       }
       else
       {
-        // Convert input image to RGB for predictions:
-        cv::Mat cvimg = jevois::rawimage::convertToCvRGB(inimg);
-      
-        // Also make a raw YUYV copy of the input image for later displays:
-        cv::Mat inimgcv = jevois::rawimage::cvImage(inimg);
-        inimgcv.copyTo(itsRawInputCv);
+	// Note: resizeInDims() could throw if the network is not ready yet.
+	try
+	{
+	  // Convert input image to RGB for predictions:
+	  cv::Mat cvimg = jevois::rawimage::convertToCvRGB(inimg);
+	  
+	  // Also make a raw YUYV copy of the input image for later displays:
+	  cv::Mat inimgcv = jevois::rawimage::cvImage(inimg);
+	  inimgcv.copyTo(itsRawInputCv);
+	  
+	  // Resize the network if desired:
+	  cv::Size nsz = netin::get();
+	  if (nsz.width != 0 && nsz.height != 0)
+	  {
+	    itsYolo->resizeInDims(nsz.width, nsz.height);
+	    
+	    if (nsz.width == cvimg.cols && nsz.height == cvimg.rows)
+	      itsNetInput = cvimg;
+	    else if (nsz.width > cvimg.cols || nsz.height > cvimg.rows)
+	      cv::resize(cvimg, itsNetInput, nsz, 0, 0, cv::INTER_LINEAR);
+	    else
+	      cv::resize(cvimg, itsNetInput, nsz, 0, 0, cv::INTER_AREA);
+	  }
+	  else itsNetInput = cvimg;
 
-        // Resize the network if desired:
-        cv::Size nsz = netin::get();
-        if (nsz.width != 0 && nsz.height != 0)
-        {
-          itsYolo->resizeInDims(nsz.width, nsz.height);
+	  cvimg.release();
+	
+	  // Launch the predictions:
+	  itsPredictFut = std::async(std::launch::async, [&](int ww, int hh)
+				     {
+				       float pt = itsYolo->predict(itsNetInput);
+				       itsYolo->computeBoxes(ww, hh);
+				       return pt;
+				     }, w, h);
+	}
+	catch (std::logic_error const & e) { }
 
-          if (nsz.width == cvimg.cols && nsz.height == cvimg.rows)
-            itsNetInput = cvimg;
-          else if (nsz.width > cvimg.cols || nsz.height > cvimg.rows)
-            cv::resize(cvimg, itsNetInput, nsz, 0, 0, cv::INTER_LINEAR);
-          else
-            cv::resize(cvimg, itsNetInput, nsz, 0, 0, cv::INTER_AREA);
-        }
-        else itsNetInput = cvimg;
-
-        cvimg.release();
-         
-        // Wait for paste to finish up:
-        paste_fut.get();
-
-        // Let camera know we are done processing the input image:
-        inframe.done();
-
-        // Launch the predictions:
-        itsPredictFut = std::async(std::launch::async, [&](int ww, int hh)
-                                   {
-                                     float pt = itsYolo->predict(itsNetInput);
-                                     itsYolo->computeBoxes(ww, hh);
-                                     return pt;
-                                   }, w, h);
+	// Wait for paste to finish up:
+	paste_fut.get();
+	
+	// Let camera know we are done processing the input image:
+	inframe.done();
       }
-
+      
       // Show processing fps:
       std::string const & fpscpu = timer.stop();
       jevois::rawimage::writeText(outimg, fpscpu, 3, h - 13, jevois::yuyv::White);
