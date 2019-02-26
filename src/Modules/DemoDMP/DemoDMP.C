aeec4222a788f35b92eb4d623cc509db32f717ef
Laurent Itti <jevois.org@gmail.com>
2019-02-26 12:20:22 -0800

    IMU digital motion processor working great except raw compass packets always contain garbage. Should use RAW of FIFO mdode for those anyways. The compass data seems to be used by the DMP to compute quaternions, but it is not properly sent out to DMP FIFO output it seems.

--- src/Modules/DemoDMP/DemoDMP.C
+++ src/Modules/DemoDMP/DemoDMP.C
@@ -103,10 +103,7 @@ class DemoDMP : public jevois::Module
       // Let camera know we are done processing the input image:
       inframe.done();
 
-      // Get one IMU reading:
-      jevois::DMPdata d = itsIMU->getDMP();
-
-      // Note: we get different packets for steps and for quaternions. So let's cache the steps:
+      // Note: we get different packets for steps, activity, and for quaternions. So let's cache the data:
       static long step_ts = 0;
       static long steps = 0;
       static std::vector<std::string> bac;
@@ -114,123 +111,179 @@ class DemoDMP : public jevois::Module
       static cv::Vec3d tvec(0.0, 0.0, 0.5); // Place cube 0.5 meters in front of the camera
       float const hsz = 0.075F; // half size of cube, in meters
       static int pickup = 0; // Number of frames remaining to show pickup message
-        
-      // ---------- Quaternions using 9 DOF:
-      if (d.header1 & JEVOIS_DMP_QUAT9)
+      long quat[3] = { 0, 0, 0 }; // Our latest quaternion
+      std::string qtype;
+      jevois::IMUrawData rd;
+      bool has_accel = false, has_gyro = false, has_cpass = false;
+      
+      // Get as many IMU readings as we can so we keep the FIFO empty:
+      jevois::DMPdata d; int npkt= 0;
+      while (npkt == 0 || itsIMU->dataReady())
       {
-        Eigen::Quaterniond q(0.0, d.fix2float(d.quat9[0]), d.fix2float(d.quat9[1]), d.fix2float(d.quat9[2]));
+        d = itsIMU->getDMP(); ++npkt;
+        
+        // ---------- Quaternions using 9 DOF:
+        if (d.header1 & JEVOIS_DMP_QUAT9)
+        {
+          quat[0] = d.quat9[0]; quat[1] = d.quat9[1]; quat[2] = d.quat9[2];
+          qtype = "Quat-9";
+        }
+
+        // ---------- Quaternions using 6 DOF:
+        if (d.header1 & JEVOIS_DMP_QUAT6)
+        {
+          quat[0] = d.quat6[0]; quat[1] = d.quat6[1]; quat[2] = d.quat6[2];
+          qtype = "Quat-6";
+        }
+
+        // ---------- Quaternions using GeoMag:
+        if (d.header1 & JEVOIS_DMP_GEOMAG)
+        {
+          quat[0] = d.geomag[0]; quat[1] = d.geomag[1]; quat[2] = d.geomag[2];
+          qtype = "GeoMag";
+        }
+
+        // ---------- Step detection:
+        if (d.header1 & JEVOIS_DMP_PED_STEPDET) { step_ts = d.stepts; steps += d.steps; }
+
+        // ---------- Activity recognition:
+        if (d.header2 & JEVOIS_DMP_ACT_RECOG) bac = d.activity2();
+      
+        // ---------- Pickup/flip detection:
+        if (d.header2 & JEVOIS_DMP_FLIP_PICKUP) pickup = 20;
+
+        // ---------- Raw accel, gyro, mag:
+        if (d.header1 & JEVOIS_DMP_ACCEL)
+        { has_accel = true; rd.ax() = d.accel[0]; rd.ay() = d.accel[1]; rd.az() = d.accel[2]; }
+
+        if (d.header1 & JEVOIS_DMP_GYRO)
+        { has_gyro = true; rd.gx() = d.gyro[0]; rd.gy() = d.gyro[1]; rd.gz() = d.gyro[2]; }
+
+        if (d.header1 & JEVOIS_DMP_CPASS)
+        { has_cpass = true; rd.mx() = d.cpass[0]; rd.my() = d.cpass[1]; rd.mz() = d.cpass[2]; }
+      }
+      
+      // ---------- Display a cube rotated by our latest quaternion:
+      if (qtype.empty() == false)
+      {
+        Eigen::Quaterniond q(0.0, d.fix2float(quat[0]), d.fix2float(quat[1]), d.fix2float(quat[2]));
 
         // Compute the scalar part, which is not returned by the IMU, to get a normalized quaternion:
         q.w() = std::sqrt(1.0 - q.squaredNorm());
         
         // Display it:
-        jevois::rawimage::writeText(outimg, jevois::sformat("Quat: x=%+09.6f y=%+09.6f z=%+09.6f", q.x(), q.y(), q.z()),
+        jevois::rawimage::writeText(outimg, qtype +
+                                    jevois::sformat(": x=%+09.6f y=%+09.6f z=%+09.6f", q.x(), q.y(), q.z()),
                                     3, h + 3, jevois::yuyv::White);
-
+        
         // Compute OpenCV rvec for use with projectPoints() below:
         // You could also compute Euler angles: Eigen::Vector3f eig_rvec = q.toRotationMatrix().eulerAngles(0, 1, 2);
         Eigen::Matrix3d eig_rmat = q.toRotationMatrix();
         cv::Mat rmat(3, 3, CV_64FC1); cv::eigen2cv(eig_rmat, rmat);
         cv::Rodrigues(rmat, rvec);
-      }
-
-      // ---------- Quaternions using 6 DOF:
-      if (d.header1 & JEVOIS_DMP_QUAT6)
-      {
-        Eigen::Quaterniond q(0.0, d.fix2float(d.quat6[0]), d.fix2float(d.quat6[1]), d.fix2float(d.quat6[2]));
-
-        // Compute the scalar part, which is not returned by the IMU, to get a normalized quaternion:
-        q.w() = std::sqrt(1.0 - q.squaredNorm());
         
-        // Display it:
-        jevois::rawimage::writeText(outimg, jevois::sformat("Quat: x=%+09.6f y=%+09.6f z=%+09.6f", q.x(), q.y(), q.z()),
-                                    3, h + 3, jevois::yuyv::White);
-
-        // Compute OpenCV rvec for use with projectPoints() below:
-        Eigen::Matrix3d eig_rmat = q.toRotationMatrix();
-        cv::Mat rmat(3, 3, CV_64FC1); cv::eigen2cv(eig_rmat, rmat);
-        cv::Rodrigues(rmat, rvec);
+        // Project axis points:
+        std::vector<cv::Point3f> axisPoints;
+        axisPoints.push_back(cv::Point3f(0.0F, 0.0F, 0.0F));
+        axisPoints.push_back(cv::Point3f(hsz * 1.5F, 0.0F, 0.0F));
+        axisPoints.push_back(cv::Point3f(0.0F, hsz * 1.5F, 0.0F));
+        axisPoints.push_back(cv::Point3f(0.0F, 0.0F, hsz * 1.5F));
+        axisPoints.push_back(cv::Point3f(hsz * 1.55F, 0.0F, 0.0F));
+        axisPoints.push_back(cv::Point3f(0.0F, hsz * 1.55F, 0.0F));
+        axisPoints.push_back(cv::Point3f(0.0F, 0.0F, hsz * 1.55F));
+        
+        std::vector<cv::Point2f> imagePoints;
+        cv::projectPoints(axisPoints, rvec, tvec, itsCamMatrix, itsDistCoeffs, imagePoints);
+        
+        // Draw axis lines
+        jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
+                                   int(imagePoints[1].x + 0.5F), int(imagePoints[1].y + 0.5F),
+                                   2, jevois::yuyv::MedPurple);
+        jevois::rawimage::writeText(outimg, "X", int(imagePoints[4].x - 4.0F), int(imagePoints[4].y - 9.5F),
+                                    jevois::yuyv::White, jevois::rawimage::Font10x20);
+        
+        jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
+                                   int(imagePoints[2].x + 0.5F), int(imagePoints[2].y + 0.5F),
+                                   2, jevois::yuyv::MedGreen);
+        jevois::rawimage::writeText(outimg, "Y", int(imagePoints[5].x - 4.0F), int(imagePoints[5].y - 9.5F),
+                                    jevois::yuyv::White, jevois::rawimage::Font10x20);
+        
+        jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
+                                   int(imagePoints[3].x + 0.5F), int(imagePoints[3].y + 0.5F),
+                                   2, jevois::yuyv::MedGrey);
+        jevois::rawimage::writeText(outimg, "Z", int(imagePoints[6].x - 4.0F), int(imagePoints[6].y - 9.5F),
+                                    jevois::yuyv::White, jevois::rawimage::Font10x20);
+        
+        // Also draw a cube:
+        std::vector<cv::Point3f> cubePoints;
+        cubePoints.push_back(cv::Point3f(-hsz, -hsz, -hsz));
+        cubePoints.push_back(cv::Point3f(hsz, -hsz, -hsz));
+        cubePoints.push_back(cv::Point3f(hsz, hsz, -hsz));
+        cubePoints.push_back(cv::Point3f(-hsz, hsz, -hsz));
+        cubePoints.push_back(cv::Point3f(-hsz, -hsz, hsz));
+        cubePoints.push_back(cv::Point3f(hsz, -hsz, hsz));
+        cubePoints.push_back(cv::Point3f(hsz, hsz, hsz));
+        cubePoints.push_back(cv::Point3f(-hsz, hsz, hsz));
+        
+        std::vector<cv::Point2f> cuf;
+        cv::projectPoints(cubePoints, rvec, tvec, itsCamMatrix, itsDistCoeffs, cuf);
+        
+        // Round all the coordinates:
+        std::vector<cv::Point> cu;
+        for (auto const & p : cuf) cu.push_back(cv::Point(int(p.x + 0.5F), int(p.y + 0.5F)));
+        
+        // Draw cube lines:
+        jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[1].x, cu[1].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[2].x, cu[2].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[3].x, cu[3].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[0].x, cu[0].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[4].x, cu[4].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[5].x, cu[5].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[6].x, cu[6].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[7].x, cu[7].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
+        jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
       }
-
-      // ---------- Display a cube rotated by our latest quaternion:
-      
-      // Project axis points:
-      std::vector<cv::Point3f> axisPoints;
-      axisPoints.push_back(cv::Point3f(0.0F, 0.0F, 0.0F));
-      axisPoints.push_back(cv::Point3f(hsz * 1.5F, 0.0F, 0.0F));
-      axisPoints.push_back(cv::Point3f(0.0F, hsz * 1.5F, 0.0F));
-      axisPoints.push_back(cv::Point3f(0.0F, 0.0F, hsz * 1.5F));
-      axisPoints.push_back(cv::Point3f(hsz * 1.55F, 0.0F, 0.0F));
-      axisPoints.push_back(cv::Point3f(0.0F, hsz * 1.55F, 0.0F));
-      axisPoints.push_back(cv::Point3f(0.0F, 0.0F, hsz * 1.55F));
-      
-      std::vector<cv::Point2f> imagePoints;
-      cv::projectPoints(axisPoints, rvec, tvec, itsCamMatrix, itsDistCoeffs, imagePoints);
-      
-      // Draw axis lines
-      jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
-                                 int(imagePoints[1].x + 0.5F), int(imagePoints[1].y + 0.5F),
-                                 2, jevois::yuyv::MedPurple);
-      jevois::rawimage::writeText(outimg, "X", int(imagePoints[4].x - 4.0F), int(imagePoints[4].y - 9.5F),
-                                  jevois::yuyv::White, jevois::rawimage::Font10x20);
-
-      jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
-                                 int(imagePoints[2].x + 0.5F), int(imagePoints[2].y + 0.5F),
-                                 2, jevois::yuyv::MedGreen);
-      jevois::rawimage::writeText(outimg, "Y", int(imagePoints[5].x - 4.0F), int(imagePoints[5].y - 9.5F),
-                                  jevois::yuyv::White, jevois::rawimage::Font10x20);
-
-      jevois::rawimage::drawLine(outimg, int(imagePoints[0].x + 0.5F), int(imagePoints[0].y + 0.5F),
-                                 int(imagePoints[3].x + 0.5F), int(imagePoints[3].y + 0.5F),
-                                 2, jevois::yuyv::MedGrey);
-      jevois::rawimage::writeText(outimg, "Z", int(imagePoints[6].x - 4.0F), int(imagePoints[6].y - 9.5F),
-                                  jevois::yuyv::White, jevois::rawimage::Font10x20);
-      
-      // Also draw a cube:
-      std::vector<cv::Point3f> cubePoints;
-      cubePoints.push_back(cv::Point3f(-hsz, -hsz, -hsz));
-      cubePoints.push_back(cv::Point3f(hsz, -hsz, -hsz));
-      cubePoints.push_back(cv::Point3f(hsz, hsz, -hsz));
-      cubePoints.push_back(cv::Point3f(-hsz, hsz, -hsz));
-      cubePoints.push_back(cv::Point3f(-hsz, -hsz, hsz));
-      cubePoints.push_back(cv::Point3f(hsz, -hsz, hsz));
-      cubePoints.push_back(cv::Point3f(hsz, hsz, hsz));
-      cubePoints.push_back(cv::Point3f(-hsz, hsz, hsz));
-      
-      std::vector<cv::Point2f> cuf;
-      cv::projectPoints(cubePoints, rvec, tvec, itsCamMatrix, itsDistCoeffs, cuf);
-      
-      // Round all the coordinates:
-      std::vector<cv::Point> cu;
-      for (auto const & p : cuf) cu.push_back(cv::Point(int(p.x + 0.5F), int(p.y + 0.5F)));
-      
-      // Draw cube lines:
-      jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[1].x, cu[1].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[2].x, cu[2].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[3].x, cu[3].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[0].x, cu[0].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[4].x, cu[4].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[5].x, cu[5].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[6].x, cu[6].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[7].x, cu[7].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[0].x, cu[0].y, cu[4].x, cu[4].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[1].x, cu[1].y, cu[5].x, cu[5].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[2].x, cu[2].y, cu[6].x, cu[6].y, 2, jevois::yuyv::LightGreen);
-      jevois::rawimage::drawLine(outimg, cu[3].x, cu[3].y, cu[7].x, cu[7].y, 2, jevois::yuyv::LightGreen);
       
       // ---------- Step detection:
-      if (d.header1 & JEVOIS_DMP_PED_STEPDET) { step_ts = d.stepts; steps += d.steps; }
       jevois::rawimage::writeText(outimg, jevois::sformat("Steps: %6u (last at %010u)", steps, step_ts),
                                   3, h + 15, jevois::yuyv::White);
 
       // ---------- Activity recognition:
-      if (d.header2 & JEVOIS_DMP_ACT_RECOG) bac = d.activity2();
-      if (bac.size()) jevois::rawimage::writeText(outimg, jevois::join(bac, ", "), 260, h + 15, jevois::yuyv::White);
+      if (bac.size()) jevois::rawimage::writeText(outimg, jevois::join(bac, ", "), 290, h + 15, jevois::yuyv::White);
       
       // ---------- Pickup/flip detection:
-      if (d.header2 & JEVOIS_DMP_FLIP_PICKUP) { pickup = 20; }
-      if (pickup) jevois::rawimage::writeText(outimg, "pickup/flip", 260, h + 3, jevois::yuyv::White);
+      if (pickup) jevois::rawimage::writeText(outimg, "pickup/flip", 214, h + 15, jevois::yuyv::White);
 
+      // ---------- Show raw accel/gyro/compass:
+      if (has_accel || has_gyro || has_cpass)
+      {
+        // Convert from raw data to scaled data:
+        jevois::IMUdata dd(rd, itsIMU->arange::get(), itsIMU->grange::get());
+
+        // Display the ones that are on:
+        if (has_accel)
+          jevois::rawimage::writeText(outimg,
+                                      jevois::sformat("Acc: x=%+06.2fg y=%+06.2fg z=%+06.2fg",
+                                                      dd.ax(), dd.ay(), dd.az()),
+                                      333, h + 3, jevois::yuyv::White);
+        if (has_gyro)
+          jevois::rawimage::writeText(outimg,
+                                      jevois::sformat("Gyr: x=%+07.1fdps y=%+07.1fdps z=%+07.1fdps",
+                                                      dd.gx(), dd.gy(), dd.gz()),
+                                      3, h + 27, jevois::yuyv::White);
+        if (has_cpass)
+          jevois::rawimage::writeText(outimg,
+                                      jevois::sformat("Mag: %+09.2fuT %+09.2fuT %+09.2fuT",
+                                                      dd.mx(), dd.my(), dd.mz()),
+                                      333, h + 27, jevois::yuyv::White);
+      }
+
+      // ---------- Show number of IMU packets processed:
+      jevois::rawimage::writeText(outimg, jevois::sformat("%03d pkts", npkt), 586, h + 3, jevois::yuyv::White);
+      
       // Decay counters for some ephemerous messages:
       if (pickup > 0) --pickup;
 
