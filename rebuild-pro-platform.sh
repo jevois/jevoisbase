40977fbb71b2863e6d2fe5f50e759abce74c21b2
Laurent Itti <jevois.org@gmail.com>
2021-09-23 18:28:03 -0700

    we only every build to staging on jevois-pro, removed the other options

--- rebuild-pro-platform.sh
+++ rebuild-pro-platform.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# USAGE: rebuild-pro-platform.sh [--staging|--microsd|--live] [cmake opts]
+# USAGE: rebuild-pro-platform.sh [cmake opts]
 
-# OPTIONS:
-# [none]    - install compiled modules to jvpkg/ for later packing by make jvpkg
-# --staging - install to microSD staging area /var/lib/jevois-microsd/
-# --microsd - install directly to microSD card inserted into host computer
-# --live    - install directly to microSD card inside running JeVois connected to host computer
-
-extra=""
-if [ "X$1" = "X--staging" ]; then extra="-DJEVOIS_MODULES_TO_STAGING=ON"; shift; fi
-if [ "X$1" = "X--microsd" ]; then extra="-DJEVOIS_MODULES_TO_MICROSD=ON"; shift; fi
-if [ "X$1" = "X--live" ]; then extra="-DJEVOIS_MODULES_TO_LIVE=ON"; shift; fi
+# Note: on jevois-pro, we only ever build for staging:
+extra="-DJEVOIS_MODULES_TO_STAGING=ON"
 
 # On JeVoisPro, limit the number of compile threads to not run out of memory:
 ncpu=`grep -c processor /proc/cpuinfo`
