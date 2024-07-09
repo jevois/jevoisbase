271672ed67966424e0df231585a210809a07f195
Laurent Itti <jevois.org@gmail.com>
2024-07-09 13:07:55 -0700

    load camera calib using Engine

--- src/Modules/FirstPython/FirstPython.py
+++ src/Modules/FirstPython/FirstPython.py
@@ -108,16 +108,13 @@ class FirstPython:
     # ###################################################################################################
     ## Load camera calibration from JeVois share directory
     def loadCameraCalibration(self, w, h):
-        cpf = pyjevois.share + "/camera/calibration{}x{}.yaml".format(w, h)
-        fs = cv2.FileStorage(cpf, cv2.FILE_STORAGE_READ)
-        if (fs.isOpened()):
-            self.camMatrix = fs.getNode("camera_matrix").mat()
-            self.distCoeffs = fs.getNode("distortion_coefficients").mat()
-            jevois.LINFO("Loaded camera calibration from {}".format(cpf))
-        else:
-            jevois.LERROR("Failed to read camera parameters from file [{}] -- IGNORED".format(cpf))
-            self.camMatrix = np.eye(3, 3, dtype=double)
-            self.distCoeffs = np.zeros(5, 1, dtype=double)
+        try:
+            self.camMatrix, self.distCoeffs = jevois.loadCameraCalibration("calibration", True)
+            jevois.LINFO("Loaded camera calibration")
+        except:
+            jevois.LERROR("Failed to load camera calibration for {}x{} -- IGNORED".format(w,h))
+            self.camMatrix = np.eye(3, 3, dtype=np.double)
+            self.distCoeffs = np.zeros(5, 1, dtype=np.double)
 
     # ###################################################################################################
     ## Detect objects within our HSV range
